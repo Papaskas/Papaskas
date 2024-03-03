82dbd93d34fd53be0b29c28e470afb98c1146247
papaskas <pavel.glugxix@gmail.com>
2024-03-03 15:49:22 +0300

    Portfolio moved to new branch

--- Portfolio/global.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.svg' {
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-  export default content;
-}
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.png';
